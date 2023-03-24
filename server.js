7e77781fb03731a483f961f0a71c133f211b2530
xiarj <webxrj@gmail.com>
2023-03-24 13:03:46 +0800

    feat: add sh

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const spawn = require("child_process").spawn;
+// 脚本地址
+const script = "deploy.sh";
+// 执行指令的地址
+const currentPath = "../myself/ppfg";
 const app = express();
 
 // 使用 bodyParser 中间件解析请求体
@@ -18,6 +22,20 @@ app.all("*", function (req, res, next) {
   //让options尝试请求快速结束
   else next();
 });
+
+function rumCommand(cmd, args, callback) {
+  var child = spawn(cmd, args, {
+    cwd: currentPath,
+  });
+  var response = "";
+  child.stdout.on("data", function (buffer) {
+    response += buffer.toString();
+  });
+  child.stdout.on("end", function () {
+    callback(response);
+  });
+}
+
 // 配置 GitHub Webhook 的路由和处理函数
 app.post("/webhook", (req, res) => {
   const eventType = req.headers["x-github-event"];
@@ -32,7 +50,10 @@ app.post("/webhook", (req, res) => {
     console.log("Pushed branch:", req.body.ref);
     console.log("Commit message:", req.body.head_commit.message);
     // 在这里编写处理 push 事件的代码
-    console.log(req.body);
+    rumCommand("sh", [script], function (txt) {
+      console.log(`在${currentPath}目录下执行了脚本${script}`);
+      console.log(txt);
+    });
     return res.send("OK");
   }
 
